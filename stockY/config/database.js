0cbc3c4cd97d5515c5a1ce5f2348dc4fb9c17596
Michael Cheung <ironmic@Michaels-MacBook-Air.local>
2022-09-01 21:47:16 -0400

    deploy to heroku

--- stockY/config/database.js
+++ stockY/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/sneakers', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
